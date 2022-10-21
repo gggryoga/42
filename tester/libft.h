9dc82696eeec073639844fe65336b56e1a2ef43b
Ryoma Ozeki <rozeki@c1r17s9.42tokyo.jp>
2022-10-21 18:50:05 +0900

    memmove

--- tester/libft.h
+++ tester/libft.h
@@ -3,6 +3,7 @@
 
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 void	*ft_calloc(size_t count, size_t size);
@@ -13,6 +14,7 @@ int	ft_isdigit(int c);
 int	ft_isprint(int c);
 void	ft_memcpy(void *dst,const void *src, size_t n);
 void	*ft_memset(void *buf,int ch,size_t n);
+void *ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *str);
